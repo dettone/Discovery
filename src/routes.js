4c14d70f08002f1e9d1a4969fa87e426caedff5f
dettone <32111175+dettone@users.noreply.github.com>
2021-05-13 21:48:03 -0300

    Discovery first navegation

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,18 @@ const routes = express();
 
 const basePath = __dirname+'/views/';
 
+const profile =  {
+    name: 'João Vitor',
+    avatar:'https://avatars.githubusercontent.com/u/32111175?s=400&v=4',
+    'monthly-budget': 3000,
+    'days-per-week': 5,
+    'hours-per-day':5,
+    'vacation-per-year':4
+}
+
 routes.get('/index',(req, res) => res.render(basePath+"index"))
 routes.get('/job',(req, res) => res.render(basePath+"job"))
 routes.get('/job-edit',(req, res) => res.render(basePath+"job-edit"))
-
+routes.get('/profile',(req,res) => res.render(basePath+"/profile",{profile}))
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -6,10 +6,11 @@ const routes = require('./routes')
 server.use(express.static('public'));
 server.use(routes);
 
+//usando template engine
 server.set('view engine', 'ejs')
 
 server.get('/',(request, response)=>{
-    return response.sendFile(__dirname+"/views/index.html")
+    return response.render(__dirname+"/views/index")
 })
 
 server.listen(3000, () => console.log("rodando..."));
